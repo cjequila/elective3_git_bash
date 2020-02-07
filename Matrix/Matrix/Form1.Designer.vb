c8d78d946d7fce20af9bff6b97d821d92c96445b
cjequila <camilleequil@gmail.com>
2020-02-07 23:23:42 +0800

    Counts 0 to 9 when start button is pressed

---
+++ Matrix/Matrix/Form1.Designer.vb
@@ -0,0 +1,360 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.a = New System.Windows.Forms.Label()
+        Me.k = New System.Windows.Forms.Label()
+        Me.p = New System.Windows.Forms.Label()
+        Me.u = New System.Windows.Forms.Label()
+        Me.v = New System.Windows.Forms.Label()
+        Me.q = New System.Windows.Forms.Label()
+        Me.l = New System.Windows.Forms.Label()
+        Me.b = New System.Windows.Forms.Label()
+        Me.w = New System.Windows.Forms.Label()
+        Me.r = New System.Windows.Forms.Label()
+        Me.m = New System.Windows.Forms.Label()
+        Me.c = New System.Windows.Forms.Label()
+        Me.x = New System.Windows.Forms.Label()
+        Me.s = New System.Windows.Forms.Label()
+        Me.n = New System.Windows.Forms.Label()
+        Me.d = New System.Windows.Forms.Label()
+        Me.y = New System.Windows.Forms.Label()
+        Me.t = New System.Windows.Forms.Label()
+        Me.o = New System.Windows.Forms.Label()
+        Me.e_seg = New System.Windows.Forms.Label()
+        Me.j = New System.Windows.Forms.Label()
+        Me.i = New System.Windows.Forms.Label()
+        Me.h = New System.Windows.Forms.Label()
+        Me.g = New System.Windows.Forms.Label()
+        Me.f = New System.Windows.Forms.Label()
+        Me.Label26 = New System.Windows.Forms.Label()
+        Me.Label27 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'a
+        '
+        Me.a.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.a.Location = New System.Drawing.Point(32, 27)
+        Me.a.Name = "a"
+        Me.a.Size = New System.Drawing.Size(30, 30)
+        Me.a.TabIndex = 0
+        '
+        'k
+        '
+        Me.k.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.k.Location = New System.Drawing.Point(32, 106)
+        Me.k.Name = "k"
+        Me.k.Size = New System.Drawing.Size(30, 30)
+        Me.k.TabIndex = 2
+        '
+        'p
+        '
+        Me.p.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.p.Location = New System.Drawing.Point(32, 145)
+        Me.p.Name = "p"
+        Me.p.Size = New System.Drawing.Size(30, 30)
+        Me.p.TabIndex = 3
+        '
+        'u
+        '
+        Me.u.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.u.Location = New System.Drawing.Point(32, 184)
+        Me.u.Name = "u"
+        Me.u.Size = New System.Drawing.Size(30, 30)
+        Me.u.TabIndex = 4
+        '
+        'v
+        '
+        Me.v.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.v.Location = New System.Drawing.Point(68, 184)
+        Me.v.Name = "v"
+        Me.v.Size = New System.Drawing.Size(30, 30)
+        Me.v.TabIndex = 9
+        '
+        'q
+        '
+        Me.q.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.q.Location = New System.Drawing.Point(68, 145)
+        Me.q.Name = "q"
+        Me.q.Size = New System.Drawing.Size(30, 30)
+        Me.q.TabIndex = 8
+        '
+        'l
+        '
+        Me.l.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.l.Location = New System.Drawing.Point(68, 106)
+        Me.l.Name = "l"
+        Me.l.Size = New System.Drawing.Size(30, 30)
+        Me.l.TabIndex = 7
+        '
+        'b
+        '
+        Me.b.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.b.Location = New System.Drawing.Point(68, 27)
+        Me.b.Name = "b"
+        Me.b.Size = New System.Drawing.Size(30, 30)
+        Me.b.TabIndex = 5
+        '
+        'w
+        '
+        Me.w.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.w.Location = New System.Drawing.Point(104, 184)
+        Me.w.Name = "w"
+        Me.w.Size = New System.Drawing.Size(30, 30)
+        Me.w.TabIndex = 14
+        '
+        'r
+        '
+        Me.r.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.r.Location = New System.Drawing.Point(104, 145)
+        Me.r.Name = "r"
+        Me.r.Size = New System.Drawing.Size(30, 30)
+        Me.r.TabIndex = 13
+        '
+        'm
+        '
+        Me.m.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.m.Location = New System.Drawing.Point(104, 106)
+        Me.m.Name = "m"
+        Me.m.Size = New System.Drawing.Size(30, 30)
+        Me.m.TabIndex = 12
+        '
+        'c
+        '
+        Me.c.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.c.Location = New System.Drawing.Point(104, 27)
+        Me.c.Name = "c"
+        Me.c.Size = New System.Drawing.Size(30, 30)
+        Me.c.TabIndex = 10
+        '
+        'x
+        '
+        Me.x.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.x.Location = New System.Drawing.Point(140, 184)
+        Me.x.Name = "x"
+        Me.x.Size = New System.Drawing.Size(30, 30)
+        Me.x.TabIndex = 19
+        '
+        's
+        '
+        Me.s.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.s.Location = New System.Drawing.Point(140, 145)
+        Me.s.Name = "s"
+        Me.s.Size = New System.Drawing.Size(30, 30)
+        Me.s.TabIndex = 18
+        '
+        'n
+        '
+        Me.n.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.n.Location = New System.Drawing.Point(140, 106)
+        Me.n.Name = "n"
+        Me.n.Size = New System.Drawing.Size(30, 30)
+        Me.n.TabIndex = 17
+        '
+        'd
+        '
+        Me.d.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.d.Location = New System.Drawing.Point(140, 27)
+        Me.d.Name = "d"
+        Me.d.Size = New System.Drawing.Size(30, 30)
+        Me.d.TabIndex = 15
+        '
+        'y
+        '
+        Me.y.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.y.Location = New System.Drawing.Point(176, 184)
+        Me.y.Name = "y"
+        Me.y.Size = New System.Drawing.Size(30, 30)
+        Me.y.TabIndex = 24
+        '
+        't
+        '
+        Me.t.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.t.Location = New System.Drawing.Point(176, 145)
+        Me.t.Name = "t"
+        Me.t.Size = New System.Drawing.Size(30, 30)
+        Me.t.TabIndex = 23
+        '
+        'o
+        '
+        Me.o.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.o.Location = New System.Drawing.Point(176, 106)
+        Me.o.Name = "o"
+        Me.o.Size = New System.Drawing.Size(30, 30)
+        Me.o.TabIndex = 22
+        '
+        'e_seg
+        '
+        Me.e_seg.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.e_seg.Location = New System.Drawing.Point(176, 27)
+        Me.e_seg.Name = "e_seg"
+        Me.e_seg.Size = New System.Drawing.Size(30, 30)
+        Me.e_seg.TabIndex = 20
+        '
+        'j
+        '
+        Me.j.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.j.Location = New System.Drawing.Point(176, 67)
+        Me.j.Name = "j"
+        Me.j.Size = New System.Drawing.Size(30, 30)
+        Me.j.TabIndex = 29
+        '
+        'i
+        '
+        Me.i.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.i.Location = New System.Drawing.Point(140, 67)
+        Me.i.Name = "i"
+        Me.i.Size = New System.Drawing.Size(30, 30)
+        Me.i.TabIndex = 28
+        '
+        'h
+        '
+        Me.h.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.h.Location = New System.Drawing.Point(104, 67)
+        Me.h.Name = "h"
+        Me.h.Size = New System.Drawing.Size(30, 30)
+        Me.h.TabIndex = 27
+        '
+        'g
+        '
+        Me.g.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.g.Location = New System.Drawing.Point(68, 67)
+        Me.g.Name = "g"
+        Me.g.Size = New System.Drawing.Size(30, 30)
+        Me.g.TabIndex = 26
+        '
+        'f
+        '
+        Me.f.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.f.Location = New System.Drawing.Point(32, 67)
+        Me.f.Name = "f"
+        Me.f.Size = New System.Drawing.Size(30, 30)
+        Me.f.TabIndex = 25
+        '
+        'Label26
+        '
+        Me.Label26.AutoSize = True
+        Me.Label26.Font = New System.Drawing.Font("Courier New", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label26.Location = New System.Drawing.Point(13, 273)
+        Me.Label26.Name = "Label26"
+        Me.Label26.Size = New System.Drawing.Size(208, 16)
+        Me.Label26.TabIndex = 30
+        Me.Label26.Text = "Equila, Camille Joglen U."
+        '
+        'Label27
+        '
+        Me.Label27.AutoSize = True
+        Me.Label27.Font = New System.Drawing.Font("Courier New", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label27.Location = New System.Drawing.Point(79, 289)
+        Me.Label27.Name = "Label27"
+        Me.Label27.Size = New System.Drawing.Size(88, 16)
+        Me.Label27.TabIndex = 31
+        Me.Label27.Text = "BS CPE 5-4"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Courier New", 8.4!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(71, 239)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(96, 22)
+        Me.Button1.TabIndex = 32
+        Me.Button1.Text = "S T A R T"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 2000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(233, 346)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label27)
+        Me.Controls.Add(Me.Label26)
+        Me.Controls.Add(Me.j)
+        Me.Controls.Add(Me.i)
+        Me.Controls.Add(Me.h)
+        Me.Controls.Add(Me.g)
+        Me.Controls.Add(Me.f)
+        Me.Controls.Add(Me.y)
+        Me.Controls.Add(Me.t)
+        Me.Controls.Add(Me.o)
+        Me.Controls.Add(Me.e_seg)
+        Me.Controls.Add(Me.x)
+        Me.Controls.Add(Me.s)
+        Me.Controls.Add(Me.n)
+        Me.Controls.Add(Me.d)
+        Me.Controls.Add(Me.w)
+        Me.Controls.Add(Me.r)
+        Me.Controls.Add(Me.m)
+        Me.Controls.Add(Me.c)
+        Me.Controls.Add(Me.v)
+        Me.Controls.Add(Me.q)
+        Me.Controls.Add(Me.l)
+        Me.Controls.Add(Me.b)
+        Me.Controls.Add(Me.u)
+        Me.Controls.Add(Me.p)
+        Me.Controls.Add(Me.k)
+        Me.Controls.Add(Me.a)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents a As Label
+    Friend WithEvents k As Label
+    Friend WithEvents p As Label
+    Friend WithEvents u As Label
+    Friend WithEvents v As Label
+    Friend WithEvents q As Label
+    Friend WithEvents l As Label
+    Friend WithEvents b As Label
+    Friend WithEvents w As Label
+    Friend WithEvents r As Label
+    Friend WithEvents m As Label
+    Friend WithEvents c As Label
+    Friend WithEvents x As Label
+    Friend WithEvents s As Label
+    Friend WithEvents n As Label
+    Friend WithEvents d As Label
+    Friend WithEvents y As Label
+    Friend WithEvents t As Label
+    Friend WithEvents o As Label
+    Friend WithEvents e_seg As Label
+    Friend WithEvents j As Label
+    Friend WithEvents i As Label
+    Friend WithEvents h As Label
+    Friend WithEvents g As Label
+    Friend WithEvents f As Label
+    Friend WithEvents Label26 As Label
+    Friend WithEvents Label27 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ Matrix/Matrix/Form1.vb
@@ -0,0 +1,356 @@
+﻿Public Class Form1
+    Dim number As Integer = 0
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Timer1.Start()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        a.BackColor = Color.Gray
+        b.BackColor = Color.Gray
+        c.BackColor = Color.Gray
+        d.BackColor = Color.Gray
+        e_seg.BackColor = Color.Gray
+
+        f.BackColor = Color.Gray
+        g.BackColor = Color.Gray
+        h.BackColor = Color.Gray
+        i.BackColor = Color.Gray
+        j.BackColor = Color.Gray
+
+        k.BackColor = Color.Gray
+        l.BackColor = Color.Gray
+        m.BackColor = Color.Gray
+        n.BackColor = Color.Gray
+        o.BackColor = Color.Gray
+
+        p.BackColor = Color.Gray
+        q.BackColor = Color.Gray
+        r.BackColor = Color.Gray
+        s.BackColor = Color.Gray
+        t.BackColor = Color.Gray
+
+        u.BackColor = Color.Gray
+        v.BackColor = Color.Gray
+        w.BackColor = Color.Gray
+        x.BackColor = Color.Gray
+        y.BackColor = Color.Gray
+
+        Select Case number
+            Case 0
+                a.BackColor = Color.Green
+                b.BackColor = Color.Green
+                c.BackColor = Color.Green
+                d.BackColor = Color.Green
+                e_seg.BackColor = Color.Green
+
+                f.BackColor = Color.Green
+                g.BackColor = Color.Gray
+                h.BackColor = Color.Gray
+                i.BackColor = Color.Gray
+                j.BackColor = Color.Green
+
+                k.BackColor = Color.Green
+                l.BackColor = Color.Gray
+                m.BackColor = Color.Gray
+                n.BackColor = Color.Gray
+                o.BackColor = Color.Green
+
+                p.BackColor = Color.Green
+                q.BackColor = Color.Gray
+                r.BackColor = Color.Gray
+                s.BackColor = Color.Gray
+                t.BackColor = Color.Green
+
+                u.BackColor = Color.Green
+                v.BackColor = Color.Green
+                w.BackColor = Color.Green
+                x.BackColor = Color.Green
+                y.BackColor = Color.Green
+
+            Case 1
+                a.BackColor = Color.Gray
+                b.BackColor = Color.Gray
+                c.BackColor = Color.Green
+                d.BackColor = Color.Gray
+                e_seg.BackColor = Color.Gray
+
+                f.BackColor = Color.Gray
+                g.BackColor = Color.Green
+                h.BackColor = Color.Green
+                i.BackColor = Color.Gray
+                j.BackColor = Color.Gray
+
+                k.BackColor = Color.Green
+                l.BackColor = Color.Gray
+                m.BackColor = Color.Green
+                n.BackColor = Color.Gray
+                o.BackColor = Color.Gray
+
+                p.BackColor = Color.Gray
+                q.BackColor = Color.Gray
+                r.BackColor = Color.Green
+                s.BackColor = Color.Gray
+                t.BackColor = Color.Gray
+
+                u.BackColor = Color.Green
+                v.BackColor = Color.Green
+                w.BackColor = Color.Green
+                x.BackColor = Color.Green
+                y.BackColor = Color.Green
+
+            Case 2
+                a.BackColor = Color.Green
+                b.BackColor = Color.Green
+                c.BackColor = Color.Green
+                d.BackColor = Color.Green
+                e_seg.BackColor = Color.Green
+
+                f.BackColor = Color.Gray
+                g.BackColor = Color.Gray
+                h.BackColor = Color.Gray
+                i.BackColor = Color.Gray
+                j.BackColor = Color.Green
+
+                k.BackColor = Color.Green
+                l.BackColor = Color.Green
+                m.BackColor = Color.Green
+                n.BackColor = Color.Green
+                o.BackColor = Color.Green
+
+                p.BackColor = Color.Green
+                q.BackColor = Color.Gray
+                r.BackColor = Color.Gray
+                s.BackColor = Color.Gray
+                t.BackColor = Color.Gray
+
+                u.BackColor = Color.Green
+                v.BackColor = Color.Green
+                w.BackColor = Color.Green
+                x.BackColor = Color.Green
+                y.BackColor = Color.Green
+
+            Case 3
+                a.BackColor = Color.Green
+                b.BackColor = Color.Green
+                c.BackColor = Color.Green
+                d.BackColor = Color.Green
+                e_seg.BackColor = Color.Green
+
+                f.BackColor = Color.Gray
+                g.BackColor = Color.Gray
+                h.BackColor = Color.Gray
+                i.BackColor = Color.Gray
+                j.BackColor = Color.Green
+
+                k.BackColor = Color.Green
+                l.BackColor = Color.Green
+                m.BackColor = Color.Green
+                n.BackColor = Color.Green
+                o.BackColor = Color.Green
+
+                p.BackColor = Color.Gray
+                q.BackColor = Color.Gray
+                r.BackColor = Color.Gray
+                s.BackColor = Color.Gray
+                t.BackColor = Color.Green
+
+                u.BackColor = Color.Green
+                v.BackColor = Color.Green
+                w.BackColor = Color.Green
+                x.BackColor = Color.Green
+                y.BackColor = Color.Green
+
+            Case 4
+                a.BackColor = Color.Green
+                b.BackColor = Color.Gray
+                c.BackColor = Color.Gray
+                d.BackColor = Color.Gray
+                e_seg.BackColor = Color.Green
+
+                f.BackColor = Color.Green
+                g.BackColor = Color.Gray
+                h.BackColor = Color.Gray
+                i.BackColor = Color.Gray
+                j.BackColor = Color.Green
+
+                k.BackColor = Color.Green
+                l.BackColor = Color.Green
+                m.BackColor = Color.Green
+                n.BackColor = Color.Green
+                o.BackColor = Color.Green
+
+                p.BackColor = Color.Gray
+                q.BackColor = Color.Gray
+                r.BackColor = Color.Gray
+                s.BackColor = Color.Gray
+                t.BackColor = Color.Green
+
+                u.BackColor = Color.Gray
+                v.BackColor = Color.Gray
+                w.BackColor = Color.Gray
+                x.BackColor = Color.Gray
+                y.BackColor = Color.Green
+
+            Case 5
+                a.BackColor = Color.Green
+                b.BackColor = Color.Green
+                c.BackColor = Color.Green
+                d.BackColor = Color.Green
+                e_seg.BackColor = Color.Green
+
+                f.BackColor = Color.Green
+                g.BackColor = Color.Gray
+                h.BackColor = Color.Gray
+                i.BackColor = Color.Gray
+                j.BackColor = Color.Gray
+
+                k.BackColor = Color.Green
+                l.BackColor = Color.Green
+                m.BackColor = Color.Green
+                n.BackColor = Color.Green
+                o.BackColor = Color.Green
+
+                p.BackColor = Color.Gray
+                q.BackColor = Color.Gray
+                r.BackColor = Color.Gray
+                s.BackColor = Color.Gray
+                t.BackColor = Color.Green
+
+                u.BackColor = Color.Green
+                v.BackColor = Color.Green
+                w.BackColor = Color.Green
+                x.BackColor = Color.Green
+                y.BackColor = Color.Green
+
+            Case 6
+                a.BackColor = Color.Green
+                b.BackColor = Color.Green
+                c.BackColor = Color.Green
+                d.BackColor = Color.Green
+                e_seg.BackColor = Color.Green
+
+                f.BackColor = Color.Green
+                g.BackColor = Color.Gray
+                h.BackColor = Color.Gray
+                i.BackColor = Color.Gray
+                j.BackColor = Color.Gray
+
+                k.BackColor = Color.Green
+                l.BackColor = Color.Green
+                m.BackColor = Color.Green
+                n.BackColor = Color.Green
+                o.BackColor = Color.Green
+
+                p.BackColor = Color.Green
+                q.BackColor = Color.Gray
+                r.BackColor = Color.Gray
+                s.BackColor = Color.Gray
+                t.BackColor = Color.Green
+
+                u.BackColor = Color.Green
+                v.BackColor = Color.Green
+                w.BackColor = Color.Green
+                x.BackColor = Color.Green
+                y.BackColor = Color.Green
+
+            Case 7
+                a.BackColor = Color.Green
+                b.BackColor = Color.Green
+                c.BackColor = Color.Green
+                d.BackColor = Color.Green
+                e_seg.BackColor = Color.Green
+
+                f.BackColor = Color.Green
+                g.BackColor = Color.Gray
+                h.BackColor = Color.Gray
+                i.BackColor = Color.Green
+                j.BackColor = Color.Gray
+
+                k.BackColor = Color.Gray
+                l.BackColor = Color.Gray
+                m.BackColor = Color.Green
+                n.BackColor = Color.Gray
+                o.BackColor = Color.Gray
+
+                p.BackColor = Color.Gray
+                q.BackColor = Color.Gray
+                r.BackColor = Color.Green
+                s.BackColor = Color.Gray
+                t.BackColor = Color.Gray
+
+                u.BackColor = Color.Gray
+                v.BackColor = Color.Gray
+                w.BackColor = Color.Green
+                x.BackColor = Color.Gray
+                y.BackColor = Color.Gray
+
+            Case 8
+                a.BackColor = Color.Green
+                b.BackColor = Color.Green
+                c.BackColor = Color.Green
+                d.BackColor = Color.Green
+                e_seg.BackColor = Color.Green
+
+                f.BackColor = Color.Green
+                g.BackColor = Color.Gray
+                h.BackColor = Color.Gray
+                i.BackColor = Color.Gray
+                j.BackColor = Color.Green
+
+                k.BackColor = Color.Green
+                l.BackColor = Color.Green
+                m.BackColor = Color.Green
+                n.BackColor = Color.Green
+                o.BackColor = Color.Green
+
+                p.BackColor = Color.Green
+                q.BackColor = Color.Gray
+                r.BackColor = Color.Gray
+                s.BackColor = Color.Gray
+                t.BackColor = Color.Green
+
+                u.BackColor = Color.Green
+                v.BackColor = Color.Green
+                w.BackColor = Color.Green
+                x.BackColor = Color.Green
+                y.BackColor = Color.Green
+
+            Case 9
+                a.BackColor = Color.Green
+                b.BackColor = Color.Green
+                c.BackColor = Color.Green
+                d.BackColor = Color.Green
+                e_seg.BackColor = Color.Green
+
+                f.BackColor = Color.Green
+                g.BackColor = Color.Gray
+                h.BackColor = Color.Gray
+                i.BackColor = Color.Gray
+                j.BackColor = Color.Green
+
+                k.BackColor = Color.Green
+                l.BackColor = Color.Green
+                m.BackColor = Color.Green
+                n.BackColor = Color.Green
+                o.BackColor = Color.Green
+
+                p.BackColor = Color.Gray
+                q.BackColor = Color.Gray
+                r.BackColor = Color.Gray
+                s.BackColor = Color.Gray
+                t.BackColor = Color.Green
+
+                u.BackColor = Color.Gray
+                v.BackColor = Color.Gray
+                w.BackColor = Color.Gray
+                x.BackColor = Color.Gray
+                y.BackColor = Color.Green
+
+
+            Case 10
+                Timer1.Stop()
+        End Select
+        number = number + 1
+    End Sub
+End Class
